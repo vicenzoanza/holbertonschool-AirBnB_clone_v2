798092ae4de97aea01f8886350b173b960f08b1b
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-03 12:03:41 -0700

    fixed _sa_instance_state

--- models/base_model.py
+++ models/base_model.py
@@ -41,8 +41,9 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
