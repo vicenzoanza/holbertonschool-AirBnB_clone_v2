f584020dff2790e64e83d0b0b6cd84b1d5f2ef8d
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-04 10:20:51 -0700

    pycodestyle for all files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,10 +52,9 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
 
     def delete(self, obj=None):
         """ delete a object from the dictionary"""
