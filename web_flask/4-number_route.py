7afe0e93955fc2fcda3c6baabe8645ddcf5756d9
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-14 14:29:30 -0700

    task 5

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''starts a script'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''displays a message'''
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''displays a message'''
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    text_with_spaces = text.replace('_', ' ')
+    return "c {}".format(text_with_spaces)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_p_text(text):
+    text_with_spaces = text.replace('_', ' ')
+    return "Python {}".format(text_with_spaces)
+
+@app.route('/number/<n>', strict_slashes=False)
+def is_num(n):
+    try:
+        n_int = int(n)
+        if isinstance(n_int, int):
+            return n
+    except ValueError:
+        pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
