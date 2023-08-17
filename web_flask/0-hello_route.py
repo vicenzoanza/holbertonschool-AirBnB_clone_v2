f32263ad1f7f3b4a17420c904e13483a6f0dec9a
vicenzoanza <viceanza2010@hotmail.com>
2023-08-17 17:30:30 -0300

    tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-'''starts a script'''
+""" Write a script that starts a Flask web application """
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    '''displays a message'''
-    return ("Hello HBNB!")
+@app.route('/')
+def hello_ab():
+    return "Hello HBNB!"
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
