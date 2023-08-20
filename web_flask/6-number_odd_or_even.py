69d043a6128fd29caffdcc6330ea984b488ab2c8
vicenzoanza <viceanza2010@hotmail.com>
2023-08-20 18:30:34 -0300

    tasks

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,6 +37,11 @@ def ab4(n):
 @app.route('/number_template/<n>', strict_slashes=False)
 def ab5(n):
     return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def ab6(n):
+    return render_template('6-number_odd_or_even.html', n=n)
   
 
 if __name__ == "__main__":
